684d231efc1ce7453319011dfe13ac924e47d0ed
Pinna Maxime <max.pinna@hotmail.fr>
2022-05-23 14:05:30 +0200

    OK : Création de la route POST /api/taches afin d'ajouter des tâches

--- app.js
+++ app.js
@@ -21,4 +21,31 @@ app.get("/api/taches", async (req, res) => {
   res.status(200).json(taches);
 });
 
+/**
+ * Ajouter une tâche
+ */
+app.post("/api/taches", async (req, res) => {
+  const payload = req.body;
+
+  // validation
+  const schema = joi.object({
+    description: joi.string().min(5).max(50).required(),
+    faite: joi.boolean().required(),
+  });
+  const { value, error } = schema.validate(payload);
+
+  // Erreur => Renvoie erreur
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+    return;
+  } else {
+    // Ajout valeur dans base de données
+    let tache = new Tache(value);
+    tache = await tache.save();
+
+    // Renvoie objet créé
+    res.status(201).json(tache);
+  }
+});
+
 module.exports = app;
